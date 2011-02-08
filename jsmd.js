66d9365dae706251f9488cdb5d3c34d20471744f
Daniel Schwen <daniel@schwen.de>
2011-02-08 11:38:28 -0600

    work on jsmd

--- jsmd.js
+++ jsmd.js
@@ -96,11 +96,163 @@ var jsmd = (function(){
     }
   }
 
+  // Simulation constructor (this contains all the important logic)
+  function Simulation(w,h) {
+    // list of atoms
+    this.atoms = [];
+
+    // list of atom types
+    this.types = [];
+
+    // interaction matrix
+    this.interaction = [];
+
+    // box dimensions
+    this.w = w;
+    this.h = h;
+
+    // linkcell data
+    this lc = {};
+
+    // neighborlist data
+    this nl = { dr : 0.0, data : [] };
+  }
+  Simulation.prototype.setInteraction = function(t,f) {
+    // set the intercation function f(r,t) for the t=[a,b] atom types
+    for( var i = 0; i < 2; ++i ) {
+      if( interaction[t[i]] === undefined ) {
+        interaction[t[i]] = [];
+      }
+      interaction[t[i]][t[1-i]] = f;
+    }
+  }
+  Simulation.prototype.setCutOff = function(rc,rp) {
+    // initialize linkcells
+    var rm = rc+rp;
+    this.rp = rp;
+
+    // number and size of cells
+    this.lc.nx = Math.floor(this.w/rm);
+    this.lc.ny = Math.floor(this.h/rm);
+    this.lc.dx = this.w/this.lc.nx;
+    this.lc.dy = this.h/this.lc.ny;
+
+    // build the 2D linkcell grid
+    var l = new Array(this.lc.nx);
+    for( var i = 0; i < this.lc.nx; ++i ) {
+      l[i] = new Array(this.lc.ny);
+    }
+    this.lc.data = l;
+  }
+  Simulation.prototype.clearLinkcell = function() {
+    // clear each cell
+    for( var i = 0; i < this.lc.nx; ++i ) {
+      for( var j = 0; j < this.lc.ny; ++j ) {
+        this.lc.data[i][j] = [];
+      }
+    }
+  }
+  Simulation.prototype.updateLinkcell = function() {
+    // clear first
+    this.clearLinkcell();
+
+    // repopulate with all atoms
+    for( i = 0; i < this.atoms.length; ++i ) {
+      this.lc.data[Math.floor(this.atoms[i].p.x/this.lc.dx)][Math.floor(this.atoms[i].p.y/this.lc.dy)].push(i);
+    }
+  }
+  Simulation.prototype.clearNeighborlist() {
+    for( i = 0; i < this.atoms.length; ++i ) {
+      this.nl.data[i] = [];
+    }
+  }
+  Simulation.prototype.updateNeighborlist = function(dr) {
+    // check if update is necessary
+    this.nl.dr += 2.0*dr;
+    if( this.nl.dr < this.rp ) { return; }
+
+    // update Linkcells first
+    this.updateLinkcell()
+
+    // clear Neigborlist
+    this.updateLinkcell()
+
+    var i,j,i2,j2,k,l,m;
+    var n = [ [0,1],[1,this.h-1],[1,0],[1,1] ]; // half the neighbors
+    // loop over all cells
+    for( i = 0; i < this.lc.nx; ++i ) {
+      for( j = 0; j < this.lc.ny; ++j ) {
+        // loop over all atoms in local cell
+        var ll = this.lc.data[i][j].length;
+        for( k = 0; k < ll; ++k ) {
+          // loop over remaining atoms in local cell
+          var ka = this.lc.data[i][j][k];
+          for( l = k+1; l < ll; ++l ) {
+            this.nl.data[ka].push(this.lc.data[i][j][l]);
+          }
+          // loop over half the neighbor cells
+          for( m = 0; m < n.length; ++m ) {
+            i2 = (i+n[m][0]) % this.lc.nx;
+            j2 = (j+n[m][1]) % this.lc.ny;
+            // loop over all atoms in that neighbor cells
+            for( l = 0; l < this.lc.data[i2][j2].length; ++l ) {
+              this.nl.data[ka].push(this.lc.data[i2][j2][l]);
+            }
+          }
+        }
+      }
+    }
+  }
+  Simulation.prototype.updateForces = function() {
+    var i,j,k;  // integer
+    var f,dr; // float
+    var rvec; // Vector
+
+    // zero forces set timestep
+    for( i = 0; i < this.atoms.length; ++i ) {
+      this.atoms[i].f.x = 0.0;
+      this.atoms[i].f.y = 0.0;
+    }
+
+    // build up forces (newtonian)
+    for( i = 0; i < this.atoms.length; ++i ) {
+      //for( j = i+1; j < atoms.length; ++j ) {     // old iteration over all other atoms
+      for( k = 0; k < this.nl.data.length; ++k ) {  // new iteration over neighborlist
+        j = this.nl.data[k];
+
+        rvec = jsmd.Vector.sub(atoms[j].p, atoms[i].p);
+        dr = rvec.len();
+        if( dr < 100.0 ) {
+          f = (phi[atoms[i].t][atoms[j].t])(dr,atoms[i].t,atoms[j].t);
+          rvec.scale(f/dr);
+          atoms[i].f.add(rvec);
+          atoms[j].f.sub(rvec);
+        }
+      }
+    }
+    // add barrier interaction
+    for( i = 0; i < atoms.length; ++i ) {
+      for( j = 0; j < barriers.length; ++j ) {
+        // find distance to barrier
+        rvec = barriers[j].dist(atoms[i].p);
+        dr = rvec.len();
+        if( dr < 1000.0 ) {
+          f = (phi[atoms[i].t][barriers[j].t])(dr);
+          //$('#mdlog').text(dr+','+f);
+          rvec.scale(f/dr);
+          atoms[i].f.add(rvec);
+        }
+      }
+    }
+  }
+
+
   // export public interface
   return {
     Atom : Atom,
     AtomType : AtomType,
     Barrier : Barrier,
-    Vector : Vector
+    Vector : Vector,
+    Simulation : Simulation
   };
 })();

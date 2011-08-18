0c44bfbc3c806e119d41a70eebb52e9e9140aa29
Daniel Schwen <daniel@schwen.de>
2011-08-17 19:54:56 -0500

    automatically repartition linkcells (in 3d)

--- linkcell3d.js
+++ linkcell3d.js
@@ -1,5 +1,5 @@
 function Linkcell3d( sim ) {
-  var l,i,j,rm;
+  var j,rm;
   
   // link to host simulation object
   this.sim = sim;
@@ -9,12 +9,17 @@ function Linkcell3d( sim ) {
   this.rm2 = rm*rm;
 
   // number and size of cells (have at least 3 cells in each direction)
-  this.nx = Math.max( 3, Math.floor(sim.ss.x/rm) );
-  this.ny = Math.max( 3, Math.floor(sim.ss.y/rm) );
-  this.nz = Math.max( 3, Math.floor(sim.ss.z/rm) );
+  this.nx = 3;
+  this.ny = 3;
+  this.nz = 3;
 
   // build the 3D linkcell grid
-  l = new Array(this.nx);
+  this.setup();
+}
+
+Linkcell3d.prototype.setup = function() {
+  // build the 3D linkcell grid
+  var i, l = new Array(this.nx);
   for( i = 0; i < this.nx; ++i ) {
     l[i] = new Array(this.ny);
     for( j = 0; j < this.ny; ++j ) {
@@ -40,13 +45,25 @@ Linkcell3d.prototype.update = function() {
   // clear first
   this.clear();
 
-  // box size can be dynamic
+  // calculate optimal linkcell size (box size can be dynamic)
+  var rm = sim.rc + sim.rp,
+      nnx = Math.max( 3, Math.floor(sim.ss.x/rm) ),
+      nny = Math.max( 3, Math.floor(sim.ss.y/rm) ),
+      nnz = Math.max( 3, Math.floor(sim.ss.z/rm) );
+
+  // do we need to redimension the linkcell grid?
+  if( this.nx != nnx || this.ny != nny || this.nz != nnz ) {
+    this.nx = nnx;
+    this.ny = nny;
+    this.nz = nnz;
+    this.setup();
+  }
+   
+  // cell size
   var dx = sim.ss.x/this.nx,
       dy = sim.ss.y/this.ny,
       dz = sim.ss.z/this.nz;
 
-  // may need to test if box shrank too far!
-      
   // repopulate with all atoms
   var lx, ly,lz, i;
   for( i = 0; i < this.sim.atoms.length; ++i ) {
@@ -55,4 +72,6 @@ Linkcell3d.prototype.update = function() {
     lz = Math.floor(this.sim.atoms[i].p.z/dz) % this.nz;
     this.data[lx][ly][lz].push(i);
   }
+  
+  // TODO: density (maximum cell occupation) could also be used as a hint for redimensioning the grid (gas!)
 }
